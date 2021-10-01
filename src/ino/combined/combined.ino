18382024f0582ac02e29ea628358d45ee8207cf3
Art at NoSQL Geeks <90031179+artofnosql@users.noreply.github.com>
2021-10-01 14:10:34 +0200

    Update combined.ino

--- src/ino/combined/combined.ino
+++ src/ino/combined/combined.ino
@@ -2,10 +2,10 @@
 const int MOTOR_PIN_ENABLE = 6, MOTOR_PIN_STEP = 5, MOTOR_PIN_DIR = 4;
 const int STP_DGR_360=200;
 const int STP_LOW = 50, STP_MED = 100,STP_HIGH = 1000;
-const int DIR_LEFT = 1, DIR_RIGHT = 0, DIR_NONE = -1;
+const int DIR_LEFT = 0, DIR_RIGHT = 1, DIR_NONE = -1;
 const int SPD_VSLOW = 30000, SPD_SLOW = 10000, SPD_MED = 5000, SPD_FAST = 500;
 const char* S_STR[] = {"LEFT", "CENTER_LEFT", "CENTER", "CENTER_RIGHT", "RIGHT"};
-const int S_LEFT=0, S_CENTER=2, S_RIGHT=4;
+const int S_LEFT=0, S_CENTER_LEFT=1, S_CENTER=2,S_CENTER_RIGHT=3,S_RIGHT=4;
 
 //Ultra-sonic sensors
 const int TRIGGER_PIN_L=8, ECHO_PIN_L=9;
@@ -13,8 +13,13 @@ const int TRIGGER_PIN_M=12, ECHO_PIN_M=10;
 const int TRIGGER_PIN_R=13, ECHO_PIN_R=11;
 const int MAX_DIST=120;
 const char* POS_STR[] = {"NONE", "LEFT", "CENTER_LEFT", "CENTER", "CENTER_RIGHT", "RIGHT", "ALL", "UNDEFINED"};
+const int POS_NONE=0, POS_LEFT=1, POS_CENTER_LEFT=2, POS_CENTER=3, POS_CENTER_RIGHT=4, POS_RIGHT=5, POS_ALL=6, POS_UNDEFINED=7;
 const char* MOV_STR[] = {"NONE", "LEFT", "RIGHT"};
 const int DIDNT_MOVE=0, MOVED_LEFT=1, MOVED_RIGHT=2;
+
+//Sampling
+const int NUM_SAMPLES_LOW=7, NUM_SAMPLES_MED=15, NUM_SAMPLES_HIGH=30;
+
 
 
 //Init the motor
@@ -124,31 +129,31 @@ int current_pos(int d_l, int d_m, int d_r) {
   int result = 7;
   
   if (seen(d_l)==0 && seen(d_m)==0 && seen(d_r)==0){
-    result=0;//NONE
+    result=POS_NONE;
   }
 
   if (seen(d_l)==1 && seen(d_m)==0 && seen(d_r)==0) {
-    result=1;//LEFT
+    result=POS_LEFT;
   }
   
   if (seen(d_l)==1 && seen(d_m)==1 && seen(d_r)==0) {
-    result=2;//CENTER_LEFT
+    result=POS_CENTER_LEFT;
   }
 
   if (seen(d_l)==0 && seen(d_m)==1 && seen(d_r)==0) {
-    result=3;//CENTER
+    result=POS_CENTER;
   }
 
   if (seen(d_l)==0 && seen(d_m)==1 && seen(d_r)==1) {
-    result=4;//CENTER_RIGHT
+    result=POS_CENTER_RIGHT;
   }
 
   if (seen(d_l)==0 && seen(d_m)==0 && seen(d_r)==1) {
-    result=5;//RIGHT
+    result=POS_RIGHT;
   }
  
   if (seen(d_l)==1 && seen(d_m)==1 && seen(d_r)==1) {
-    result=6;//ALL
+    result=POS_ALL;
   }
 
   return result;
@@ -164,20 +169,21 @@ int detect_movement(){
   //There is a movement if a state transistions from left to right or right to left within a period of n seconds
   
   //Take some samples
-  int NUM_SAMPLES = 10;
-  
-  long samples[NUM_SAMPLES];
+  int num_samples = NUM_SAMPLES_MED;
+  long samples[num_samples];
 
   boolean from_left=false;
   boolean from_right=false;
   int start_idx=0;
   
-  for (int i=0; i < NUM_SAMPLES; i++) {
+  for (int i=0; i < num_samples; i++) {
     long d_l = dist(TRIGGER_PIN_L, ECHO_PIN_L);
-    delay(5);
+    delay(1);
     long d_m = dist(TRIGGER_PIN_M, ECHO_PIN_M);
-    delay(5);
+    delay(1);
     long d_r = dist(TRIGGER_PIN_R, ECHO_PIN_R);
+    delay(1);
+    
     Serial.println("[ " + String(d_l) + "," + String(d_m) + "," + String(d_r) + " ]");
     
     int cpos=current_pos(d_l,d_m,d_r);
@@ -195,8 +201,6 @@ int detect_movement(){
         start_idx=i;
       }
     }
-         
-    delay(50);
   }
 
   //Start to scan for the opposite direction
@@ -229,23 +233,24 @@ int detect_movement(){
  * Detect the last position of the observer
  */
 int detect_pos(){
- //Take 10 samples -- We might do more fancy stuff with them
- long samples[10];
+ long num_samples = NUM_SAMPLES_LOW;
+ long samples[num_samples];
  int cpos=0;
 
- for (int i=0; i < 10; i++) {
+ for (int i=0; i < num_samples; i++) {
     
     long d_l = dist(TRIGGER_PIN_L, ECHO_PIN_L);
-    delay(10);
+    delay(1);
     long d_m = dist(TRIGGER_PIN_M, ECHO_PIN_M);
-    delay(10);
+    delay(1);
     long d_r = dist(TRIGGER_PIN_R, ECHO_PIN_R);
-    
+    delay(1);
+
+  stepper_enable(true);
+
     Serial.println("[ " + String(d_l) + "," + String(d_m) + "," + String(d_r) + " ]");
     cpos=current_pos(d_l,d_m,d_r);
     samples[i] = cpos;
-
-    delay(80);  
  }
 
  return cpos;
@@ -259,10 +264,16 @@ int follow_me(int s, int m) {
 
   //Default value = undefined
   int s_o = -1;
-  int MAX_DEGREE=60;
+  int BASE_DEGREE=30;
+  int MAX_DEGREE=2*BASE_DEGREE;
 
   stepper_enable(true);
 
+  //Ignore the intermediate states
+  if (s == S_CENTER_LEFT) { step(degree_to_steps(BASE_DEGREE),DIR_RIGHT,SPD_VSLOW); s=S_CENTER;}
+  if (s == S_CENTER_RIGHT) { step(degree_to_steps(BASE_DEGREE),DIR_LEFT,SPD_VSLOW); s=S_CENTER;}
+  
+  //TODO: Simplify
   if (s == S_LEFT) {
     if (m == MOVED_LEFT) {s_o = S_LEFT;}
     if (m == DIDNT_MOVE) {step(degree_to_steps(MAX_DEGREE),DIR_RIGHT,SPD_VSLOW);s_o = S_CENTER;}
@@ -285,19 +296,82 @@ int follow_me(int s, int m) {
   return s_o;
 }
 
+
+/**
+ * Adapt the position by staring at the observer
+ */
+int stare_at_me(int s, int p) {
+
+  int s_o = s;
+  int BASE_DEGREE=30;
+
+  //DEBUG
+  //Serial.println(p);
+  //Serial.println(s);
+
+  stepper_enable(true);
+
+  //TODO: Simplify
+  if (s == S_LEFT) {
+    if (p == POS_LEFT) {s_o=S_LEFT;}
+    if (p == POS_CENTER_LEFT) {step(degree_to_steps(BASE_DEGREE),DIR_RIGHT,SPD_VSLOW);s_o=S_CENTER_LEFT;}
+    if (p == POS_CENTER) {step(degree_to_steps(2*BASE_DEGREE),DIR_RIGHT,SPD_VSLOW);s_o=S_CENTER;}
+    if (p == POS_CENTER_RIGHT) {step(degree_to_steps(3*BASE_DEGREE),DIR_RIGHT,SPD_VSLOW);s_o=S_CENTER_RIGHT;}
+    if (p == POS_RIGHT) {step(degree_to_steps(4*BASE_DEGREE),DIR_RIGHT,SPD_VSLOW),s_o=S_RIGHT;}
+  }
+
+  if (s == S_CENTER_LEFT) {
+    if (p == POS_LEFT) {step(degree_to_steps(BASE_DEGREE),DIR_LEFT,SPD_VSLOW), s_o=S_LEFT;}
+    if (p == POS_CENTER_LEFT) {s_o=S_CENTER_LEFT;}
+    if (p == POS_CENTER) {step(degree_to_steps(BASE_DEGREE),DIR_RIGHT,SPD_VSLOW);s_o=S_CENTER;}
+    if (p == POS_CENTER_RIGHT) {step(degree_to_steps(2*BASE_DEGREE),DIR_RIGHT,SPD_VSLOW);s_o=S_CENTER_RIGHT;}
+    if (p == POS_RIGHT) {step(degree_to_steps(3*BASE_DEGREE),DIR_RIGHT,SPD_VSLOW);s_o=S_RIGHT;}
+  }
+
+  if (s == S_CENTER) {
+    if (p == POS_LEFT) {step(degree_to_steps(2*BASE_DEGREE),DIR_LEFT,SPD_VSLOW),s_o=S_LEFT;}
+    if (p == POS_CENTER_LEFT) {step(degree_to_steps(BASE_DEGREE),DIR_LEFT,SPD_VSLOW);s_o=S_CENTER_LEFT;}
+    if (p == POS_CENTER) {s_o=S_CENTER;}
+    if (p == POS_CENTER_RIGHT) {step(degree_to_steps(BASE_DEGREE),DIR_RIGHT,SPD_VSLOW);s_o=S_CENTER_RIGHT;}
+    if (p == POS_RIGHT) {step(degree_to_steps(2*BASE_DEGREE),DIR_RIGHT,SPD_VSLOW);s_o=S_RIGHT;}
+  }
+
+  if (s == S_CENTER_RIGHT) {
+    if (p == POS_LEFT) {step(degree_to_steps(3*BASE_DEGREE),DIR_LEFT,SPD_VSLOW); s_o=S_LEFT;}
+    if (p == POS_CENTER_LEFT) {step(degree_to_steps(2*BASE_DEGREE),DIR_LEFT,SPD_VSLOW);s_o=S_CENTER_LEFT;}
+    if (p == POS_CENTER) {step(degree_to_steps(BASE_DEGREE),DIR_LEFT,SPD_VSLOW);s_o=S_CENTER;}
+    if (p == POS_CENTER_RIGHT) {s_o=S_CENTER_RIGHT;}
+    if (p == POS_RIGHT) {step(degree_to_steps(BASE_DEGREE),DIR_RIGHT,SPD_VSLOW);s_o=S_RIGHT;}
+  }
+
+  if (s == S_RIGHT) {
+    if (p == POS_LEFT) {step(degree_to_steps(4*BASE_DEGREE),DIR_LEFT,SPD_VSLOW);s_o=S_LEFT;}
+    if (p == POS_CENTER_LEFT) {step(degree_to_steps(3*BASE_DEGREE),DIR_LEFT,SPD_VSLOW);s_o=S_CENTER_LEFT;}
+    if (p == POS_CENTER) {step(degree_to_steps(2*BASE_DEGREE),DIR_LEFT,SPD_VSLOW);s_o=S_CENTER;}
+    if (p == POS_CENTER_RIGHT) {step(degree_to_steps(BASE_DEGREE),DIR_LEFT,SPD_VSLOW);s_o=S_CENTER_RIGHT;}
+    if (p == POS_RIGHT) {s_o=S_RIGHT;}
+  }
+
+  stepper_enable(false);
+  
+  return s_o;
+}
+
 //Main loop
 int s_curr=S_CENTER;
 
 void loop() {
-    
+
+  //Calibrate
+  //TODO
+       
   //Detect the movement and react
   int m = detect_movement();
   Serial.println(MOV_STR[m]);
   s_curr = follow_me(s_curr,m);
   
   //Adjust
-  //int p = detect_pos();
-  //Serial.println(POS_STR[p]);
-    
-  delay(50);
+  int p = detect_pos();
+  Serial.println(POS_STR[p]);
+  s_curr = stare_at_me(s_curr,p);
 }
